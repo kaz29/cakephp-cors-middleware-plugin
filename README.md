e3c4b5e0de7832a4b75fc3d5dad88dc7fa2af2c9
Kaz WATANABE <kwatanabe@heartbeats.jp>
2021-03-07 16:26:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# CorsMiddleware plugin for CakePHP
+
+## Installation
+
+You can install this plugin into your CakePHP application using [composer](https://getcomposer.org).
+
+The recommended way to install composer packages is:
+
+```
+composer require kaz29/cakephp-cors-middleware-plugin
+```
+
+## Minimum setup
+
+- Add the following configuration into config/app_local.php.
+
+```
+    'App' => [
+        'cors' => [
+            'allowUrls' => [
+                'https://example.com', // your web site urls
+                'https://app.example.com',
+            ],
+        ],
+    ],
+```
+
+OR 
+
+When injecting with environment variables, it looks like this...
+```
+    'App' => [
+        'cors' => [
+            'allowUrls' => explode(',', env('CORS_ALLOW_URL', ''))
+        ],
+    ],
+```
+
+- Add load middleware setting into Application::middleware method.
+
+```
+    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
+    {
+        $middlewareQueue
+            ->add(new CorsMiddleware(Configure::read('App.cors')))  // Add this line
+            // Catch any exceptions in the lower layers,
+            // and make an error page/response
+            ->add(new ErrorHandlerMiddleware(Configure::read('Error')))
+```
+## Customize settings
+
+```
+    'App' => [
+        'cors' => [
+            'allowUrls' => [
+                'https://example.com', // your web site urls
+                'https://app.example.com',
+            ],
+        ],
+        'allowMethods' => [
+            'GET',
+            'POST',
+            'HEAD',
+            'OPTIONS',
+            'PUT',
+            'DELETE',
+        ],
+        'allowHeaders' => [
+            'Accept-Language',
+            'content-type',
+            'Accept',
+            'Origin',
+            'Cookie',
+            'Content-Length',
+            'Authorization',
+        ],
+        'exposeHeaders' => [],
+    ],
+```
+
+
+## Author
+
+Kazuhiro Watanabe - cyo [at] mac.com - [https://twitter.com/kaz_29](https://twitter.com/kaz_29)
+
+## License
+
+CorsMiddleware plugin for CakePHP is licensed under the MIT License - see the LICENSE file for details
---
+++ src/Middleware/CorsMiddleware.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace CorsMiddleware\Middleware;
+
+use Laminas\Diactoros\Response\EmptyResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CorsMiddleware implements MiddlewareInterface
+{
+    protected $config = [
+        'statusCode' => 200,
+        'allowUrls' => [],
+        'allowMethods' => [
+            'GET',
+            'POST',
+            'HEAD',
+            'OPTIONS',
+            'PUT',
+            'DELETE',
+        ],
+        'allowHeaders' => [
+            'Accept-Language',
+            'content-type',
+            'Accept',
+            'Origin',
+            'Cookie',
+            'Content-Length',
+            'Authorization',
+        ],
+        'exposeHeaders' => [],
+    ];
+
+    /**
+     * Constructor
+     *
+     * @param array $config The options to use.
+     * @see self::$config
+     */
+    public function __construct(array $config = [])
+    {
+        $this->config = $config + $this->config;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $allow_url = $this->checkAllowURL($request);
+        if ($allow_url !== false) {
+            if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+                return $this->allowCors(new EmptyResponse($this->config['statusCode']), $allow_url);
+            }
+        }
+
+        $response = $handler->handle($request);
+
+        if ($allow_url !== false) {
+            $response = $this->allowCors($response, $allow_url);
+        }
+
+        return $response;
+    }
+
+    protected function checkAllowURL(ServerRequestInterface $request)
+    {
+        $origin = $request->getHeader('Origin');
+        if (!$origin) {
+            return false;
+        }
+
+        foreach ($origin as $url) {
+            foreach ($this->config['allowUrls'] as $allow_url) {
+                if ($url === $allow_url) {
+                    return $allow_url;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    protected function allowCors(ResponseInterface $response, $allow_url): ResponseInterface
+    {
+        $response = $response
+            ->withHeader('Access-Control-Allow-Origin', $allow_url)
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Methods', implode(',', $this->config['allowMethods']))
+            ->withHeader('Access-Control-Allow-Headers', implode(',', $this->config['allowHeaders']));
+
+        if (is_array($this->config['exposeHeaders'])) {
+            $response = $response
+                ->withHeader('Access-Control-Expose-Headers', implode(',', $this->config['exposeHeaders']));
+        }
+
+        return $response;
+    }
+}
